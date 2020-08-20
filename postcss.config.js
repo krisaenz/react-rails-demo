29077cefe8810abfc98c2174567dffbcb8e481cc
Kristian Saenz <ksaenz@brikit.com>
2020-08-19 17:49:23 -0700

    Fixing tailwind path in postcss config cause I moved it

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     plugins: [
-      require('tailwindcss')('./app/assets/stylesheets/tailwind.config.js'),
+      require('tailwindcss')('./tailwind.config.js'),
       require('autoprefixer'),
       require('postcss-import'),
       require('postcss-flexbugs-fixes'),
@@ -12,4 +12,3 @@ module.exports = {
       })
     ]
   }
-  
